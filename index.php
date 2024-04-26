109caf884e64981ec2733041c47051586011cbdd
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-26 18:26:02 +0000

    added image

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.yellow-purple.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.orange-purple.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
@@ -27,7 +27,7 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" alt="dice" width="250" />
+        <img src="./images/positive_negative.jpg" alt="positive_negative" width="250" />
       </div>
       <h3>Select Positive or Negative.
       </h3>
