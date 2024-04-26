f702fc0bab8a0185076cd8c61ec4ab9a53715ae9
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-26 18:22:43 +0000

    added favicon and code

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.yellow-purple.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Number Guessing Game, in PHP</title>
+  <title>Positive or Negative #, in PHP</title>
 </head>
 
 <body>
@@ -22,27 +22,31 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Number Guessing Game, in PHP</span>
+        <span class="mdl-layout-title">Positive or Negative, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
         <img src="./images/dice.gif" alt="dice" width="250" />
       </div>
-      <h3>Select a number between 1-6:
+      <h3>Choose Positive or Negative
       </h3>
       <div class="page-content-php">
         <form action="answer.php" method="POST">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="number-guessed">
-            <label class="mdl-textfield__label" for="number-guessed">Enter a number ...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
+          <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="positive">
+            <input type="radio" id="positive" class="mdl-radio__button" name="options" value="1" checked>
+            <span class="mdl-radio__label">Positive</span>
+          </label>
+          <br />
+          <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="negative">
+            <input type="radio" id="negative" class="mdl-radio__button" name="options" value="1">
+            <span class="mdl-radio__label">Negative</span>
+          </label>
           <br />
           <br />
           <!-- Accent-colored raised button with ripple -->
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
-            Guess
+            Generate
           </button>
         </form>
       </div>
