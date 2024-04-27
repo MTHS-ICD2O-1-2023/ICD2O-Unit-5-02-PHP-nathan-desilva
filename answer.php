72a5a408b024363487f9d4d261e806698015dbbe
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-27 02:34:45 +0000

    edited php

--- answer.php
+++ answer.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Number Guessing Game, in PHP" />
+  <meta name="description" content="Positive or Negative #, in PHP" />
   <meta name="keywords" content="mths, ics2o" />
   <meta name="author" content="Nathan De Silva" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.orange-purple.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Number Guessing Game, in PHP</title>
+  <title>Positive or Negative #, in PHP</title>
 </head>
 
 <body>
@@ -22,12 +22,12 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Number Guessing Game, in PHP</span>
+        <span class="mdl-layout-title">Positive and Negative #, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" alt="dice" width="250" />
+        <img src="./images/positive_negative.jpg" alt="positive_negative" width="250" />
       </div>
       <div class="page-content-php">
         <div name="number-guessed">
@@ -38,22 +38,18 @@
             <div class="mdl-layout__left">
               <?php
 
-              if (isset($_POST["number-guessed"])) {
-                $randomNumber = floor(rand(6, 1));
-                $numberGuessed = intval($_POST["number-guessed"]);
+                $positiveButton = $_POST ["options"];
+                $randomNumber = floor(rand(1, 6));
+                $randomNegativeNumber = floor(rand(1, 6)) * -1;
 
                 // process
-                if ($numberGuessed == $randomNumber) {
+                if ($positiveButton == true) {
                   // output
-                  echo "You guessed: " . $numberGuessed . ", and got the correct number which was: " . $randomNumber;
-                }
-
-                // process
-                if ($numberGuessed != $randomNumber) {
-                  // output
-                  echo "You guessed: " . $numberGuessed . ", ";
-                  echo "the correct number was " . $randomNumber;
-                }
+                  echo "You selected Positive and your number is: " . $randomNumber . ".";
+                //process
+                } elseif ($positiveButton == false) {
+                // output
+                echo "You selected Negative and your number is: " . $randomNegativeNumber . ". ";
               }
 
               ?>
